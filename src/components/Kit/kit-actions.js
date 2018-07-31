a9e903b455ed2924e5fdecc1538f81fa982181ac
njmyers <17010801+njmyers@users.noreply.github.com>
2018-07-31 11:47:03 -0400

    added more documentation for actions/reducers clearer to read and understand the eventual goals

--- src/components/Kit/kit-actions.js
+++ src/components/Kit/kit-actions.js
@@ -1,15 +1,34 @@
 // @flow
 
+type Keys =
+  | 'positionX'
+  | 'positionY'
+  | 'scale'
+  | 'rotation'
+  | 'layer'
+  | 'orientation';
+
+export type Action = {
+  type: '@KIT/UPDATE_PROPERTY',
+  key: Keys,
+  value: number,
+};
+
+type Validator = (value: number) => boolean;
+
 /**
  * Use this to create kit action creators
  * Sounds silly but it's just specifying which prop to update
- * Also you can pass in a validator function to give us warnings
+ * Also you can pass in a validator function to give us warnings in development
+ * Do we want stricter warnings?
  */
-const kitActionCreator = (validator) => (key) => (value: number) => {
-  if (process.env.NODE_ENV !== 'production' && !validator(value)) {
+const kitActionCreator = (validator: Validator) => (key: Keys) => (
+  value: number
+): Action => {
+  if (process.env.NODE_ENV !== 'production' && !validator((value: number))) {
     console.warn(`You are trying to set a kit property with an invalid value.
       key: ${key}
-      value: ${value}
+      value: ${value.toString()}
       validator: ${validator.name}`);
   }
 
@@ -20,27 +39,50 @@ const kitActionCreator = (validator) => (key) => (value: number) => {
   };
 };
 
-// validators
+/**
+ * Validates position
+ * We may need more validation here at the moment lets' keep it above 0
+ */
+const isValidPositionX: Validator = (value) => value > 0;
 
-/** Future home of position validator */
-const isValidPositionX = (value) => value > 0;
+/**
+ * Validates position
+ * We may need more validation here at the moment lets' keep it above 0
+ */
+const isValidPositionY: Validator = (value) => value > 0;
 
-/** Future home of position validator */
-const isValidPositionY = (value) => value > 0;
+/**
+ * Validates the scale parameter
+ * Scale should be between this range
+ * This is a whole number percentage value
+ * Will end up like transform: scale(value / 100)
+ */
+const isValidScale: Validator = (value) => value > 20 && value < 200;
 
-/** Scale should be between this range */
-const isValidScale = (value) => value > 20 && value < 200;
+/**
+ * Validates a degree as a value between 0 and 360;
+ * We can manipulate these in css using transform: rotate(deg)
+ */
+const isValidRotation: Validator = (value) => value >= 0 && value <= 360;
 
-/** Degree as whole number */
-const isValidRotation = (value) => value >= 0 && value <= 360;
+/**
+ * Validates layer value
+ * We can add in later if we need a minimum zIndex to keep above the map
+ * This just checks it is below maximum zIndex value
+ * https://stackoverflow.com/questions/491052/minimum-and-maximum-value-of-z-index
+ */
+const isValidLayer: Validator = (value) => value >= 0 && value <= 16777271;
 
-/** Max zIndex value */
-const isValidLayer = (value) => value >= 0 && value <= 16777271;
+/**
+ * This validates a correct orientation value
+ * Orientation is either 0 or -1.
+ * Seems weird but the best way to do css flip is with transform: rotateX(-1)
+ * So we can simply add these values with our scale values
+ * https://css-tricks.com/snippets/css/flip-an-image/
+ */
+const isValidOrientation: Validator = (value) => value === 0 || value === 1;
 
-/** future orientation validator */
-const isValidOrientation = (value) => true;
-
-// These are the actual action creators
+// These are the actual action creator functions
 
 const updatePositionX = kitActionCreator(isValidPositionX)('positionX');
 
@@ -53,17 +95,6 @@ const updateRotation = kitActionCreator(isValidRotation)('rotation');
 const updateLayer = kitActionCreator(isValidLayer)('layer');
 
 const updateOrientation = kitActionCreator(isValidOrientation)('orientation');
-
-type Fn<T> = (...args: Array<any>) => T;
-type ExtractReturn = <T>(Fn<T>) => T;
-
-export type Action =
-  | $Call<ExtractReturn, typeof updatePositionX>
-  | $Call<ExtractReturn, typeof updatePositionY>
-  | $Call<ExtractReturn, typeof updateScale>
-  | $Call<ExtractReturn, typeof updateRotation>
-  | $Call<ExtractReturn, typeof updateLayer>
-  | $Call<ExtractReturn, typeof updateOrientation>;
 
 export {
   updatePositionX,
--- src/components/Kit/kit-reducer.js
+++ src/components/Kit/kit-reducer.js
@@ -1,37 +1,37 @@
 import type { Action } from './kit-actions';
 import type { Action as MapAction } from '../Map/map-actions';
 
-export type Kit = {
+export type State = {
   /** a number id specifying which piece of kit (mapped to graphics/metadata) */
-  typeID: number,
+  +typeID: number,
   /** number specifying the category of the kit */
-  category: number,
+  +category: number,
   /** whole number pixel value for position on y axis */
-  positionX: number,
+  +positionX: number,
   /** whole number pixel value for position on x axis */
-  positionY: number,
+  +positionY: number,
   /** scale is expressed as a whole number percentage */
-  scale: number,
+  +scale: number,
   /** orientation is a boolean expressing whether or not the image is flipped */
-  orientation: boolean,
+  +orientation: number,
   /** rotation is a whole number in degrees ie max 360 */
-  rotation: number,
+  +rotation: number,
   /** layer is a whole number expressing z-index style properties */
-  layer: number,
+  +layer: number,
 };
 
-export const initialKit: Kit = {
+export const initialKit: State = {
   typeID: 0,
   category: 0,
   positionX: 0,
   positionY: 0,
   scale: 100,
-  orientation: false,
+  orientation: 0,
   rotation: 0,
   layer: 1,
 };
 
-function kitReducer(state: Kit = initialKit, action: Action & MapAction) {
+function kitReducer(state: State = initialKit, action: Action & MapAction) {
   switch (action.type) {
     case '@KIT/UPDATE_PROPERTY':
       return {
